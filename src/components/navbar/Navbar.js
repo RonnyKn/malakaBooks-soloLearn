d26a09d0389544fe3768e52d37aca4d782af8102
RonnyKn <ronnykurniawn@gmail.com>
2023-01-04 15:20:27 +0700

    adding navbar content

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -2,7 +2,29 @@ import "./Navbar.css"
 import React from "react"
 
 const Navbar = () => {
-  return <div>Navbar</div>
+  return (
+    <nav>
+      <div className="container nav-container">
+        <div className="nav-logo">
+          <span>M</span>
+          <h3>Malaka Books</h3>
+        </div>
+        <div className="nav-menus">
+          <ul>
+            <li>For Everyone</li>
+            <li>Benefit</li>
+            <li>Preview</li>
+            <li>About</li>
+          </ul>
+        </div>
+        <div className="nav-download">
+          <button type="button" className="btn">
+            Download
+          </button>
+        </div>
+      </div>
+    </nav>
+  )
 }
 
 export default Navbar
