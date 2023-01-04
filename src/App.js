9ee8211cf0767511da4f9e38e2a459cf906f2f39
RonnyKn <ronnykurniawn@gmail.com>
2023-01-04 15:21:51 +0700

    adding Preview pages and route

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Navbar from "./components/navbar/Navbar"
 import About from "./pages/about/About"
 import Benefits from "./pages/benefits/Benefits"
 import ForEveryone from "./pages/forEveryone/ForEveryone"
+import Preview from "./pages/preview/Preview"
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route exact path="/" element={<ForEveryone />} />
         <Route path="/benefits" element={<Benefits />} />
         <Route path="/about" element={<About />} />
+        <Route path="/preview" element={<Preview />} />
       </Routes>
       <Footer />
     </BrowserRouter>
---
+++ src/pages/preview/Preview.js
@@ -0,0 +1,8 @@
+import "./Preview.css"
+import React from "react"
+
+const Preview = () => {
+  return <div>Preview</div>
+}
+
+export default Preview
