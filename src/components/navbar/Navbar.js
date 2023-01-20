51852218d5c9b0e5d232fd41f27d85a7d7cc81e0
RonnyKn <ronnykurniawn@gmail.com>
2023-01-20 13:54:59 +0700

    adding active class to Navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -2,16 +2,17 @@ import "./Navbar.css"
 import React, { useState } from "react"
 import { GoThreeBars } from "react-icons/go"
 import { MdOutlineClose } from "react-icons/md"
-import { Link } from "react-router-dom"
+import { NavLink } from "react-router-dom"
 
 const Navbar = ({ navbar_data: { title_app, navigation } }) => {
   const [isNavShowing, setIsNavShowing] = useState(false)
+  const [isActive, setIsActive] = useState("/")
 
   const scrollTop = () => {
     window.scroll(0, 0)
     setIsNavShowing(false)
   }
-
+  console.log(isActive)
   return (
     <nav>
       <div className="container nav-container">
@@ -22,9 +23,18 @@ const Navbar = ({ navbar_data: { title_app, navigation } }) => {
         <div className={`nav-menus ${isNavShowing ? "nav-show" : "nav-hide"}`}>
           <ul>
             {navigation?.map((val, idx) => (
-              <Link to={val?.to} onClick={scrollTop} key={idx}>
-                <li> {val?.text} </li>
-              </Link>
+              <NavLink
+                to={val?.to}
+                onClick={() => {
+                  scrollTop()
+                  setIsActive(`${val?.to}`)
+                }}
+                key={idx}
+              >
+                <li className={val?.to === isActive ? "nav-active" : null}>
+                  {val?.text}
+                </li>
+              </NavLink>
             ))}
             <li>
               <button type="button" className="btn nav-btn">
